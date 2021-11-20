db764c411e5df12dc62ccafbf5c32a2f21cdabd7
Matt Seligman <matthew.modelitup@gmail.com>
2021-11-20 07:52:09 +0000

    Plot the path for the parade based on directional steps to x, y coordinates.

---
+++ x-marks-the-perfect-shot.js
@@ -0,0 +1,16 @@
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north']  
+
+function finalPosition(moves){
+  let paradePath = [0,0];
+
+  for(let move of moves){
+    if(move === 'north'){ paradePath[1]++ }
+    else if (move === 'south'){ paradePath[1]--; }
+    else if (move === 'east'){ paradePath[0]++; }
+    else if (move === 'west'){ paradePath[0]--; }
+  }
+  return paradePath;
+}
+
+console.log(finalPosition(moves));
+//goal numeral output [-1,4]
