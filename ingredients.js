47fc4f4c48b933e8be302690fc6cba2e77665366
Matt Seligman <matthew.modelitup@gmail.com>
2021-11-21 07:22:31 +0000

    Iterating over arrays with 3 types of loops

---
+++ ingredients.js
@@ -0,0 +1,22 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+let arrayLength = ingredients.length;
+let i = 0;
+
+// Write a while loop that prints out the contents of ingredients:
+while (i < arrayLength){
+  console.log(ingredients[i]);
+  i++;
+}
+
+// Write a for loop that prints out the contents of ingredients:
+for(i = 0;  i < arrayLength; i++){
+  console.log(ingredients[i]);
+}
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+let reverseIngredients = ingredients.reverse();
+
+for(i = 0;  i < arrayLength; i++){
+  console.log(reverseIngredients[i]);
+}
