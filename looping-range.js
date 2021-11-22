99563dc696d9bc5b34ca085abab5c9a7ee0a4e7a
Matt Seligman <matthew.modelitup@gmail.com>
2021-11-22 00:30:52 +0000

    Loop through a Start to End with a Step increment.

---
+++ looping-range.js
@@ -0,0 +1,17 @@
+function range(start, end, step) { 
+  var theRange = [] 
+  if (step > 0){
+    for(let i = start; i <= end; i = i + step){
+      //console.log(i);
+      theRange.push(i);
+    }
+  } else { 
+    theRange = []; 
+  }
+  return theRange;
+}
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
+console.log(range(-5, 2, -3));
