556f52e822e9411ac468741ef32c64a520c17242
Matt Seligman <matthew.modelitup@gmail.com>
2021-11-24 03:40:30 +0000

    Find a html element by ID

---
+++ find-by-id.js
@@ -0,0 +1,19 @@
+/*
+	TODO:
+  
+  	1. Create an empty variable, `elm`
+  	2. Find an element with the ID `info`
+    3. Set `elm` to the variable that you found
+    4. Alert the inner text of the element with `alert` function, it should alert the text "JavaScript on The DOM"
+*/
+let em = null;
+em = document.getElementById('info').innerHTML;
+alert(em);
+
+/* Notes
+
+document.getElementById('identifier');
+document.getElementsByTagName('p');
+document.getElementsByClassName('class-name');
+
+*/
