e7a17e146b4955ebd9d5c96ca5af853120e92eb6
Matt Seligman <matthew.modelitup@gmail.com>
2021-11-24 07:46:08 +0000

    Select input by ID using the querySelector DOM method. Set the placeholder attribute to custom text.

---
+++ adding-new-attributes.js
@@ -0,0 +1,6 @@
+// TODO:
+// 1. Find the input element by the ID using the `querySelector` DOM method
+// 2. Set the `placeholder` attribute to some text value
+
+let placeholder = "Add your text here.";
+document.querySelector('#name').setAttribute('placeholder', placeholder);
