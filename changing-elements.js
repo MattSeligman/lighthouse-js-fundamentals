1a2552000694540f0da2c90fcdbc6b9e9e18fe47
Matt Seligman <matthew.modelitup@gmail.com>
2021-11-24 07:18:45 +0000

    Changing the innerHTML selected by an ID with JavaScript.

---
+++ changing-elements.js
@@ -0,0 +1,11 @@
+/*
+	TODO:
+  
+  	1. Create an empty variable, `elm`
+  	2. Find an element with the ID `info` and set the variable `elm` with its value
+    3. Change the contents of that elements to say `JavaScript Rocks!`
+*/
+
+let elm = null;
+elm = document.getElementById("info");
+elm.innerHTML = "JavaScript Rocks!";
